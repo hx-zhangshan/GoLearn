962efa88f4f6d0857177dde74178b17cdb13bd0c
wx_zhangshan <zs260262911@sin.cn>
2017-12-27 09:35:16 +0800

    提交 并行排序  第三章

--- src/cmd/pipelinedemo/main.go
+++ src/cmd/pipelinedemo/main.go
@@ -1,13 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"pipeline"
+	"time"
 )
 
 func main() {
-	p :=pipeline.Merge(pipeline.InMeSort( pipeline.ArraySource(2, 3, 1, 7, 6)),
-		pipeline.InMeSort( pipeline.ArraySource(4,3,8,23,11,44,9)))
+	const filename = "small.txt"
+	const n = 10000
+	start := time.Now().Nanosecond()
+	//create a file
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	//想要先生成 一个数据源 然后写入文件
+	p := pipeline.RandomSource(n)
+	//这里用一个buffer.io来进行缓冲优化速度
+	writer := bufio.NewWriter(file)
+	pipeline.WriteSink(writer, p)
+	/**
+	当用到bufio的时候应该注意
+	记得一定要进行刷新 不然缓存区里面的 东西写不尽，文件不能完全的保留
+	*/
+	writer.Flush()
+	//从新打开文件读取 数据
+	file, err = os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	p = pipeline.ReaderSource(bufio.NewReader(file), -1)
+	count := 0
+	for v := range p {
+		fmt.Println(v)
+		count++
+		if count >= 10 {
+			break
+		}
+	}
+	end := time.Now().Nanosecond()
+	fmt.Println("用的时间是：", end-start)
+}
+func mergeName() {
+	p := pipeline.Merge(pipeline.InMeSort(pipeline.ArraySource(2, 3, 1, 7, 6)),
+		pipeline.InMeSort(pipeline.ArraySource(4, 3, 8, 23, 11, 44, 9)))
 	//for {
 	//	if num, ok := <-p; ok {
 	//		fmt.Println(num)
--- src/pipeline/nodes.go
+++ src/pipeline/nodes.go
@@ -1,6 +1,11 @@
 package pipeline
 
-import "sort"
+import (
+	"encoding/binary"
+	"io"
+	"math/rand"
+	"sort"
+)
 
 //做一个函数 把数组放到通道里面 只从里面 拿东西
 func ArraySource(a ...int) <-chan int {
@@ -27,7 +32,9 @@ func InMeSort(in <-chan int) <-chan int {
 		for v := range in {
 			p = append(p, v)
 		}
+		//运用 标准库中的sort进行排序
 		sort.Ints(p)
+		//排序之后再放进通道里面
 		for _, v := range p {
 			out <- v
 		}
@@ -37,17 +44,17 @@ func InMeSort(in <-chan int) <-chan int {
 	return out
 }
 
-//进行合并 第三部
+//进行合并 第三部 核心的算法
 func Merge(in1, in2 <-chan int) <-chan int {
 	out := make(chan int)
 	go func() {
 		v1, ok1 := <-in1
 		v2, ok2 := <-in2
 		/*
-		分析对于两种情况的业务  对于 计算的影响
+			分析对于两种情况的业务  对于 计算的影响
 		*/
 		for ok1 || ok2 {
-			if !ok2 || (ok1&&v1 <= v2) {
+			if !ok2 || (ok1 && v1 <= v2) {
 				out <- v1
 				v1, ok1 = <-in1
 			} else {
@@ -58,4 +65,61 @@ func Merge(in1, in2 <-chan int) <-chan int {
 		close(out)
 	}()
 	return out
+}
+
+//读文件  改造方法进行分块读取  块的尺寸是-1的时候全部读取
+func ReaderSource(reader io.Reader, chuakSize int) <-chan int {
+	out := make(chan int, 8)
+	go func() {
+		buffer := make([]byte, 8)
+		bytesread := 0
+		for {
+			n, err := reader.Read(buffer)
+			bytesread += n
+			if n > 0 {
+				//当n>0是说明 还有 数据要读取  把buffer中的 数据放入通道
+				//binary.BigEndian是 二进制工具类的大端实现 int等于cpu的位数与unint等是 兄弟类型？
+				v := int(binary.BigEndian.Uint64(buffer))
+				out <- v
+			}
+			if err != nil || (chuakSize != -1 && bytesread > chuakSize) {
+				break
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+//写数据进入 通道
+func WriteSink(writer io.Writer, in <-chan int) {
+	for v := range in {
+		buffer := make([]byte, 8)
+		binary.BigEndian.PutUint64(buffer, uint64(v))
+		writer.Write(buffer)
+	}
+}
+
+//随机生成数字的 资源
+func RandomSource(count int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < count; i++ {
+			out <- rand.Int()
+		}
+		close(out)
+	}()
+	return out
+}
+
+//节点之间的归并 两两归并
+func MergeN(inputs ...<-chan int) <-chan int {
+	if len(inputs) == 1 {
+		//说明 只有一个元素
+		return inputs[0]
+	}
+	m := len(inputs) / 2
+	//merge inputs[0..m) and inputs(m..end]
+	return Merge(MergeN(inputs[0:m]...),
+		MergeN(inputs[m:]...))
 }
